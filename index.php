5a0a15dede340fde2800a6a7240facafe44dacf3
Anthony Williams <pull.rebase>
2016-04-14 10:26:51 -0600

    Added code to deployment

--- index.php
+++ index.php
@@ -6,18 +6,22 @@
 	</head>
 	<body>
 		<h1>Arthur of bloomberg article targeting high networth influencers</h1>
-
-		<p>Profile of Arthur
-			profileId
-			nameId
-			mediaId
-			dateId
-			timeId
-			@userId
-		</p>
+		
+		<p>Profile of Arthur</p>
+		<ul>
+			<li>profileId</li>
+			<li>nameId</li>
+			<li>mediaId</li>
+			<li>dateId</li>
+			<li>timeId</li>
+			<li>@userId</li>
+		</ul>
 		<h2>Conceptual model</h2>
-			profile writes article:		m-to-n
-			profile shares article:		m-to-n
+		<p>Model of Arthur</p>
+		<ul>
+			<li>profile writes article:		m-to-n</li>
+			<li>profile shares article:		m-to-n</li>
+		</ul>
 
 
 
