08a6a9e99dba7b43470ea7d5b022395051c24cbb
Anthony Williams <pull.rebase>
2016-04-14 10:05:42 -0600

    Added code to deployment

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"
+		<title>Bloomberg-article-page</title>
+	</head>
+	<body>
+		<h1>Arthur of bloomberg article targeting high networth influencers</h1>
+
+		<p>Profile of Arthur
+			profileId
+			nameId
+			mediaId
+			dateId
+			timeId
+			@userId
+		</p>
+		<h2>Conceptual model</h2>
+			profile writes article:		m-to-n
+			profile shares article:		m-to-n
+
+
+
+
+	</body>
+</html>
