3f4ae2814be70b838fca54186c471993375b2193
Anthony Williams <pull.rebase>
2016-04-19 17:01:20 -0600

    Added aurthor php file to bloomberg article page

---
+++ php/aurthor.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Edu\Cnm\awilliams144\bloomberg;
+
+/**
+ * Small Cross Section of a Bloomberg Articles
+ *
+ * These Articles can be considered a small example of what services like Bloomberg 
+ *
+ */
+
+
+public function getProfileId() {
+
+		 return($this-> profileId);
+
+}
