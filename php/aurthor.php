e744ab14e4bb8ff6748d85090d504fcc3d5f197a
Anthony Williams <pull.rebase>
2016-04-20 11:18:23 -0600

    Added aurthor php file to bloomberg article page

--- php/aurthor.php
+++ php/aurthor.php
@@ -2,16 +2,23 @@
 
 namespace Edu\Cnm\awilliams144\bloomberg;
 
+require_once("autoload.php");
+
 /**
  * Small Cross Section of a Bloomberg Articles
  *
- * These Articles can be considered a small example of what services like Bloomberg.com
- *
+ * These Articles can be considered a small example of what services like Bloomberg.com provide.
+ * @author Anthony Williams <awilliams144@cnm.edu
+ * @version 2.0.0
  */
 
 class Profile {
 
 	private $profileId;
+	/**
+	 * actual textual content of this article
+	 * @var string $profileContent
+	 */
 
 	/**
 	 * accessor method for profile id
@@ -19,7 +26,7 @@ class Profile {
 	 **/
 
 	public function getProfileId() {
-		return ($this->profileId);
+		return($this->profileId);
 	}
 
 
@@ -35,6 +42,8 @@ class Profile {
 		if($newProfileId <= 0) {
 					throw(new \RangeException("profile id is not positive"));
 		}
+		// convert and store this profile id
+		$this->profileId = $newProfileId;
 	}
 }
 
