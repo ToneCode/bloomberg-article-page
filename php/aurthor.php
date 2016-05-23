472899d2c23fa3437f7b4fc6992a4e3f30fb593e
Anthony Williams <mylifeassurance@aol.com>
2016-05-23 11:02:30 -0600

    Added formatting

--- php/aurthor.php
+++ php/aurthor.php
@@ -3,7 +3,6 @@
 namespace Edu\Cnm\awilliams144\bloomberg;
 
 
-
 /**
  * Small Cross Section of a Bloomberg Articles
  *
@@ -12,8 +11,8 @@ namespace Edu\Cnm\awilliams144\bloomberg;
  * @version 2.0.0
  **/
 
-class Profile implements \JsonSerializable  {
-				use ValidateProfileId;
+class Profile implements \JsonSerializable {
+	use ValidateProfileId;
 	private $profileId;
 	/**
 	 * actual textual content of this article
@@ -23,23 +22,24 @@ class Profile implements \JsonSerializable  {
 	 * constructor for this Profile
 	 * @param int $newProfileId id of the Profile wrote this article
 	 **/
-public function __construct(int $newProfileId = null) {
-    try {
-		 $newProfileId = $this->profileId($newProfileId);
-	   }	catch(\InvalidArgumentException $invalidArgument) {
-		 // rethrow the exception to the caller
-		 throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
-	 }	catch(\RangeException $range) {
-		 //rethrow the exception to the caller
-		 throw(new \RangeException($range->getMessage(), 0, $range));
-	 }	catch(\TypeError $typeError) {
-		 //rethrow the exception to the caller
-		 throw(new \TypeError($typeError->getMessage(), 0, $typeError));
-	 }	catch(\Exception $exception){
-		 //rethrow the exception to the caller
-		 throw(new \Exception($exception->getMessage(), 0, $exception));
-	 }
-}
+	public function __construct(int $newProfileId = null) {
+		try {
+			$newProfileId = $this->profileId($newProfileId);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			// rethrow the exception to the caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			//rethrow the exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			//rethrow the exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			//rethrow the exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
+
 	/**
 	 * inserts this Profile into mySQL
 	 *
@@ -47,53 +47,57 @@ public function __construct(int $newProfileId = null) {
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws  \TypeError if $pdo is not a PDO connection object
 	 **/
-	public function insert(\PDO $pdo){
+	public function insert(\PDO $pdo) {
 		// enforce the profileId is null (i.e., don't insert a profile that already exist)
-			if($this->profileId !== null){
-				throw(new \ PDOException("not a new profile"));
-				}
-			}
-	}
-	/**
-	 *
-	 *
-	 * accessor method for profile id
-	 * @return int value of profile id
-	 **/
-
-	public function getProfileId() {
-		return($this->profileId);
-	}
-
-
-
-	/**
-	 * Mutator method for profile id
-	 *
-	 * @param int $newProfileId new value of profile id
-	 * @throws \RangeException if $newProfileId is not positive
-	 * @throws \TypeError if $newProfileId is not an integer
-	 **/
-	public function setProfileId(int $newProfileId = null) {
-		// verify the profile id is positive
-		if($newProfileId <= 0) {
-					throw(new \RangeException("profile id is not positive"));
+		if($this->profileId !== null) {
+			throw(new \ PDOException("not a new profile"));
 		}
-		// convert and store this profile id
-		$this->profileId = $newProfileId;
 	}
-	/**
-	 * formats the state variables for JSON serialization
-	 *
-	 * @return array resulting state variables to serialize
-	 **/
-	public function jsonSerialize() {
-		// TODO: Implement jsonSerialize() method.
-		$fields = get_object_vars($this);
-		$fields["profileId"] = intval($this->profileId->format("U")) * 1000;
-		return($fields);
+}
 
+/**
+ *
+ *
+ * accessor method for profile id
+ * @return int value of profile id
+ **/
+
+public
+function getProfileId() {
+	return ($this->profileId);
+}
+
+
+/**
+ * Mutator method for profile id
+ *
+ * @param int $newProfileId new value of profile id
+ * @throws \RangeException if $newProfileId is not positive
+ * @throws \TypeError if $newProfileId is not an integer
+ **/
+public
+function setProfileId(int $newProfileId = null) {
+	// verify the profile id is positive
+	if($newProfileId <= 0) {
+		throw(new \RangeException("profile id is not positive"));
 	}
+	// convert and store this profile id
+	$this->profileId = $newProfileId;
+}
+
+/**
+ * formats the state variables for JSON serialization
+ *
+ * @return array resulting state variables to serialize
+ **/
+public
+function jsonSerialize() {
+	// TODO: Implement jsonSerialize() method.
+	$fields = get_object_vars($this);
+	$fields["profileId"] = intval($this->profileId->format("U")) * 1000;
+	return ($fields);
+
+}
 }
 
 
